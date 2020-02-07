c9010096b70b00424d2df57b185253748c1a6b34
batiichela <49419052+batiichela@users.noreply.github.com>
2020-02-07 11:34:44 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app container">
+      <div className="jumbotron">
+        <h1 className="text-center">Registro de Nuevos Usuarios</h1>
+        <hr/><br/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
